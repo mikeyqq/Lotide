5f1a1e27805bc5d48f562dbdf77a6b13be3968c0
Anchen Huang <acmhuang@gmail.com>
2019-06-24 21:40:33 +0000

    Added Stop Signs and Check Marks

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 let assertEqual = function(actual, expected) {
-if (actual === expected) {
-console.log('Assertion Passed:' + actual + ' === ' + expected);
-} else {
-console.log('Assertion Failed:' + actual + ' !== ' + expected);
-}
+  if (actual === expected) {
+    console.log('✅✅✅' +  ' Assertion Passed:' + actual + ' === ' + expected);
+  } else {
+    console.log('🛑🛑🛑' + ' Assertion Failed:' + actual + ' !== ' + expected);
+  }
 };
 
 assertEqual('Lighthouse', 'abc');
-assertEqual(1, 3);
+assertEqual(1, 1);
