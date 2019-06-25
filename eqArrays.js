c5f217b995f6afe5911b154e335086135c5a7522
Anchen Huang <acmhuang@gmail.com>
2019-06-25 20:44:52 +0000

    Comparing two different arrays

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+let output = [];
+
+let eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
