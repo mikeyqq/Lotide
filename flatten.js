aee7e2994468c5c0870d27c3ab3cce6e756048c1
Anchen Huang <acmhuang@gmail.com>
2019-06-26 12:58:35 +0000

    Using concat to flat array in array

---
+++ flatten.js
@@ -0,0 +1,33 @@
+let assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅✅  Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} !==  ${arrTwo}`);
+  }
+};
+
+
+let eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+let flatten = function(arr) {
+  let results = [];
+  for ( let i = 0; i < arr.length; i++ ) {
+    results = results.concat(arr[i]);
+  }
+  return results;
+};
+
+
+console.log((flatten([1, 2, [3, 4], 5, [6]])));
