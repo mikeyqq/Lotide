9fffb35f03763f7f98504127876a170b4651954f
Anchen Huang <acmhuang@gmail.com>
2019-06-26 21:54:49 +0000

    Finding the letter positions of strings

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+let assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅✅  Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} !==  ${arrTwo}`);
+  }
+};
+
+
+let eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const letterPositions = function(sentence) {
+const results = {};
+sentence = sentence.replace(/\s/g, '')
+
+for(let i = 0; i < sentence.length; i++){
+  if(!results[sentence[i]]) {
+    results[sentence[i]] = [];
+  } 
+ results[sentence[i]].push(i)
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+
+
