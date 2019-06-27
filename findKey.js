51cb92e38353a805066d028c30d5438376b18863
Anchen Huang <acmhuang@gmail.com>
2019-06-27 23:45:39 +0000

    comparing keys

---
+++ findKey.js
@@ -0,0 +1,38 @@
+let assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+
+
+const findKey = function (obj, callback) {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key
+    }
+  }
+  return undefined
+}
+
+let found = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2)
+
+
+assertEqual(found, 'noma');
+
+
+
+
+
+
+
+
