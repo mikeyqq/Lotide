6dbe24c759ff618da7e4434f2c3716ea54249f57
Anchen Huang <acmhuang@gmail.com>
2019-06-27 11:09:35 +0000

    Comparing Objects and their keys to retrieve true or false

---
+++ eqObjects.js
@@ -0,0 +1,50 @@
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+let eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+
+    for (let key in object1) {
+      if ((Array.isArray(object1[key]) && Array.isArray(object2[key]))) {
+        if (eqArrays(object1[key], object2[key]) === false) {
+          return false;
+        }
+              
+      } else {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc));
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2));
