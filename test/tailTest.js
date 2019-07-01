81e6cfccbbe2d374e853d7b516f8031d5fe1ff25
Anchen Huang <acmhuang@gmail.com>
2019-07-01 18:47:26 +0000

    incorporated mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,24 @@
 
 const tail = require('../tail');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+//const assertArraysEqual = require('../assertArraysEqual');
+
+describe("#tail", () => {
+  it("returns 2, 3, 4, 5, 6 for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5, 6]), [2, 3, 4, 5, 6]);
+  });
+
+  it("returns 'hi' for ['tester', 'hi']", () => {
+    assert.deepEqual(tail(['tester', 'hi']), ['hi']); 
+  });
+
+});
 
 
-//TEST CODE
-assertArraysEqual(tail([]), undefined);
+
+
+
+/*//TEST CODE
 assertArraysEqual(tail([1, 2, 3, 4, 5, 6]), [2, 3, 4, 5, 6]);
 assertArraysEqual(tail(['tester', 'hi']), ['hi']);
-
+assertArraysEqual(tail([]), 0);*/
