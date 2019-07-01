0e49fd126d6238441791016b91711efbc17b55ef
Anchen Huang <acmhuang@gmail.com>
2019-07-01 16:46:12 +0000

    refactor and with two requires for comparing two arrays returing true or false

---
+++ test/eqArraysTest.js
@@ -0,0 +1,9 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+//TEST CODE FOR ARRAYS TO COMPARE TWO DIFFERENT ARRAYS
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
