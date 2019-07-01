5b53c2fc4eb65ebe8cc9fee687b9b459ffd1bc68
Anchen Huang <acmhuang@gmail.com>
2019-07-01 16:52:10 +0000

    changes made to comment

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,7 +2,7 @@ const eqArrays = require('../eqArrays');
 const assertArraysEqual = require('../assertArraysEqual');
 
 
-//TEST CODE FOR ARRAYS TO COMPARE TWO DIFFERENT ARRAYS
+//TEST CODE FOR ARRAYS TO COMPARE TWO DIFFERENT ARRAYS to see if actual returns true or false
 assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
