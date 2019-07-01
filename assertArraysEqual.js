5f251012d3f442d9c9a931103f9e0b11e1a3455a
Anchen Huang <acmhuang@gmail.com>
2019-07-01 17:06:51 +0000

    refactor out and included module export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays')
+
 let assertArraysEqual = function(arrOne, arrTwo) {
   if (eqArrays(arrOne, arrTwo)) {
     console.log(`✅✅✅  Assertion Passed: ${arrOne} === ${arrTwo}`);
@@ -7,20 +9,5 @@ let assertArraysEqual = function(arrOne, arrTwo) {
 };
 
 
-let eqArrays = function(arrOne, arrTwo) {
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] !== arrTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
