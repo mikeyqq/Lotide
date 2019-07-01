b07023544df7c2825b72f6466be0bb4255752f1b
Anchen Huang <acmhuang@gmail.com>
2019-07-01 20:23:25 +0000

    added module exports

--- without.js
+++ without.js
@@ -38,10 +38,12 @@ results += source[i];
   return results;
 }
 
+module.exports = without;
 
+/*
 console.log(without([1, 2, 3], [1])); 
 console.log(without(["1", "2", "3"], [1, 2, "3"]));
 const words = ["hello", "world", "lighthouse"];
 without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);*/
