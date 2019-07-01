885f09293b2de77b21d2727430f467f99d18807e
Anchen Huang <acmhuang@gmail.com>
2019-07-01 19:08:34 +0000

    added mocha and chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,9 +1,17 @@
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+
+describe("#head", () => {
+it ('returns 1 for 1', ()  => {
+  assert.deepEqual(assertEqual(1, 1));
+})
+})
+
 
 // TEST CODE
-assertEqual(1, 1);
+/*assertEqual(1, 1);
 assertEqual(2, 1);
 assertEqual('2', 1);
 assertEqual('2', '2');
-assertEqual('2', '1');
+assertEqual('2', '1');*/
 
