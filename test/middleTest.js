ee6c46a898c31c77e815d188c451f1503c6b02b1
Anchen Huang <acmhuang@gmail.com>
2019-07-01 18:50:53 +0000

    added mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,12 @@
-const assertArraysEqual = require('../assertArraysEqual')
+//const assertArraysEqual = require('../assertArraysEqual')
 const middle = require('../middle')
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+  it("returns [3] for [1, 3, 4]", () => {
+    assert.deepEqual(middle([1, 3, 4]), [3]);
+  });
+});
 
 //TEST CODE
-assertArraysEqual(middle([1, 3, 4]), [3]);
+//assertArraysEqual(middle([1, 3, 4]), [3]);
