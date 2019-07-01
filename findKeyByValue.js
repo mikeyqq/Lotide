508c4b750159cc0a3189c47ae1146d6dfb6e2761
Anchen Huang <acmhuang@gmail.com>
2019-07-01 20:18:57 +0000

    added module exports

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -23,6 +23,9 @@ const bestTVShowsByGenre = {
   drama:  "The Wire"
 };
 
+module.exports = findKeyByValue;
+
+/*
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);*/
 
