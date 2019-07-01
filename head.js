22278efa38281b669eb159fd4345339c8b5ec7f1
Anchen Huang <acmhuang@gmail.com>
2019-07-01 13:48:05 +0000

    added module.exports = head;

--- head.js
+++ head.js
@@ -1,16 +1,8 @@
-let assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
 
-
-let head = function(valueOne = []) {
+let head = function(valueOne) {
   return valueOne[0];
 };
 
-assertEqual(head([]), undefined);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
+//runs successfully.
